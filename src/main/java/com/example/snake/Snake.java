c9c783030047bb2986601ba822a13b15fb51938a
El Miri Martin <elmiri.martin@gmail.com>
2026-04-14 12:55:21 +0200

    kezdődik

---
+++ src/main/java/com/example/snake/Snake.java
@@ -0,0 +1,20 @@
+package com.example.snake;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Snake {
+    
+    int size = 5;
+    int speed = 3;
+    Rectangle bodyNode = new Rectangle(10, 10);
+    Color color = Color.GREEN;
+
+    public static void initSnake() {
+
+    }
+
+    private static void initSnakeBody() {
+        
+    }
+}
