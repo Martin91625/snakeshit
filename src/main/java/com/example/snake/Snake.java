66287fbfd3dc3e0d886adc8d232ac2763ed59aa1
El Miri Martin <elmiri.martin@gmail.com>
2026-04-15 14:02:03 +0200

    maga a kígyó kódolása mocsok nehéz lesz...

--- src/main/java/com/example/snake/Snake.java
+++ src/main/java/com/example/snake/Snake.java
@@ -41,8 +41,4 @@ public class Snake {
         this.pos[0] = posx;
         this.pos[1] = posy;
     }
-
-    
-
-    
 }
---
+++ src/main/java/com/example/snake/SnakeControl.java
@@ -0,0 +1,18 @@
+package com.example.snake;
+
+public class SnakeControl {
+    
+    /* Snake snake = new Snake();
+    public void moveSnake() {
+        double[] pos = snake.getPos();
+        String dir = snake.getDirection();
+        
+        switch (dir) {
+            case "RIGHT" -> snake.setPos(pos[0] + gameSpeed * dt, pos[1]);
+            case "LEFT" -> snake.setPos(pos[0] - gameSpeed * dt, pos[1]);
+            case "UP" -> snake.setPos(pos[0], pos[1] - gameSpeed * dt);
+            case "DOWN" -> snake.setPos(pos[0], pos[1] + gameSpeed * dt);
+            default -> throw new AssertionError();
+        }
+    } */
+}
